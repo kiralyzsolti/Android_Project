ed458ff777bab758bd875a34e9cba8564eff9bad
kiralyzsolti <62137925+kiralyzsolti@users.noreply.github.com>
2021-02-08 06:36:47 +0200

    Delete

--- AndroidProject/app/src/main/java/com/example/androidproject/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.androidproject.utils
-
-class Constants {
-    companion object{
-        const val BASE_URL = "https://ratpark-api.imok.space"
-        const val PER_PAGE = 5
-    }
-}
