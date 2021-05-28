8589b841cedd8d0a14fba68caed0f781dca6b329
kiralyzsolti <62137925+kiralyzsolti@users.noreply.github.com>
2021-05-29 02:45:14 +0300

    RecyclerView for MainFragment

--- Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/MainFragment.kt
+++ Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/MainFragment.kt
@@ -1,20 +1,25 @@
-package com.example.where_to_eat
+package com.example.where_to_eat.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.where_to_eat.R
 
 
-class DetailFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        return view
     }
 
 }
--- Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/ProfileFragment.kt
+++ Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/ProfileFragment.kt
@@ -1,10 +1,11 @@
-package com.example.where_to_eat
+package com.example.where_to_eat.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.where_to_eat.R
 
 
 class ProfileFragment : Fragment() {
