90ec7aa0cc9b875855ccdf702c0f4a202464412d
kiralyzsolti <62137925+kiralyzsolti@users.noreply.github.com>
2021-05-29 04:45:59 +0300

    Design for layouts

--- Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/profile/ProfileFragment.kt
+++ Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
-package com.example.where_to_eat.fragments
+package com.example.where_to_eat.fragments.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.where_to_eat.R
 
 
@@ -16,6 +18,10 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.findViewById<Button>(R.id.p_update).setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_updateFragment)
+        }
 
         return view;
     }
---
+++ Where_to_eat/app/src/main/java/com/example/where_to_eat/fragments/profile/RegistrationFragment.kt
@@ -0,0 +1,23 @@
+package com.example.where_to_eat.fragments.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.where_to_eat.R
+
+
+class RegistrationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+
+        return  view
+    }
+    
+}
